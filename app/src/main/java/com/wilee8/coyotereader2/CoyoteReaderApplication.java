5551f79202d48f729e5e34ab2ed3d2b813893d4f
Allan Yaklin <allan.yaklin@gmail.com>
2016-01-28 07:40:37 -0700

    Added ACRA crash reports

---
+++ app/src/main/java/com/wilee8/coyotereader2/CoyoteReaderApplication.java
@@ -0,0 +1,31 @@
+package com.wilee8.coyotereader2;
+
+import android.app.Application;
+
+import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+@ReportsCrashes(mailTo = "allan.yaklin@gmail.com",
+	customReportContent = {ReportField.BRAND,
+						   ReportField.PHONE_MODEL,
+						   ReportField.ANDROID_VERSION,
+						   ReportField.USER_COMMENT,
+						   ReportField.STACK_TRACE
+	},
+	mode = ReportingInteractionMode.DIALOG,
+	resDialogText = R.string.crash_dialog_text,
+	resDialogTitle = R.string.crash_dialog_title,
+	resDialogCommentPrompt = R.string.crash_dialog_comment_prompt,
+	resDialogOkToast = R.string.crash_dialog_ok_toast
+)
+
+public class CoyoteReaderApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		ACRA.init(this);
+	}
+}
