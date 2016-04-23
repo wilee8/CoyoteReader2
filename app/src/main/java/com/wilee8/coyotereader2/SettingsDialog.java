d48ee21fff126208711d682847c6cf4a2e3457b8
Allan Yaklin <allan.yaklin@gmail.com>
2016-04-22 21:57:45 -0600

    Fixed blank dialog window title still appearing for preferences pre-marshmallow

--- app/src/main/java/com/wilee8/coyotereader2/SettingsDialog.java
+++ app/src/main/java/com/wilee8/coyotereader2/SettingsDialog.java
@@ -1,6 +1,8 @@
 package com.wilee8.coyotereader2;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
@@ -25,6 +27,16 @@ public class SettingsDialog extends DialogFragment {
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		mWidth = getArguments().getInt("width", -1);
 		super.onCreate(savedInstanceState);
+	}
+
+	@NonNull
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		// PreferenceFragmentCompat hides the window title in Marshmallow for some reason
+		// Create a title view in the fragment layout, but hide the window title for pre-marshmallow
+		Dialog dialog =  super.onCreateDialog(savedInstanceState);
+		dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		return dialog;
 	}
 
 	@Nullable
