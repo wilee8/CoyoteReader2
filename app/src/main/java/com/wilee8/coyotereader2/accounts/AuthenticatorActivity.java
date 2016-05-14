b48134187dbc9d33b6a5f39ffd1c473388c9605f
Allan Yaklin <allan.yaklin@gmail.com>
2016-05-13 19:21:47 -0600

    Changed OAuth to use external browser instead of WebView

--- app/src/main/java/com/wilee8/coyotereader2/accounts/AuthenticatorActivity.java
+++ app/src/main/java/com/wilee8/coyotereader2/accounts/AuthenticatorActivity.java
@@ -2,31 +2,27 @@ package com.wilee8.coyotereader2.accounts;
 
 import android.accounts.AccountAuthenticatorActivity;
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.util.ArrayMap;
 import android.view.View;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 import com.wilee8.coyotereader2.BuildConfig;
 import com.wilee8.coyotereader2.R;
 import com.wilee8.coyotereader2.retrofitservices.AuthHeaderInterceptor;
 import com.wilee8.coyotereader2.retrofitservices.InoreaderRxService;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLDecoder;
 import java.net.URLEncoder;
-import java.util.HashMap;
 import java.util.Map;
 
 import okhttp3.OkHttpClient;
 import okhttp3.ResponseBody;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
-import rx.Observable;
 import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
@@ -39,19 +35,17 @@ import rx.schedulers.Schedulers;
 public class AuthenticatorActivity extends AccountAuthenticatorActivity {
 
 	// UI references.
-	private View    mProgressView;
-	private WebView mWebView;
+	private View mProgressView;
 
-	private Boolean mAuthInProgress;
+	private Intent mIntent;
 
-	private InoreaderRxService       mService;
-	private Subscription             mLoginSubscription;
-	private Observable<ResponseBody> mRequest;
+	private InoreaderRxService mService;
+	private Subscription       mLoginSubscription;
 
 	@SuppressWarnings("FieldCanBeLocal")
 	private static String OAUTH_URL       = "https://www.inoreader.com/oauth2/auth";
 	@SuppressWarnings("FieldCanBeLocal")
-	private static String REDIRECT_URI    = "http://localhost";
+	private static String REDIRECT_URI    = "https://coyotereaderrss.androidapp";
 	@SuppressWarnings("FieldCanBeLocal")
 	private static String OPTIONAL_SCOPES = "read write";
 
@@ -74,62 +68,8 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity {
 
 		setContentView(R.layout.activity_authenticator);
 		mProgressView = findViewById(R.id.login_progress);
-		mWebView = (WebView) findViewById(R.id.webv);
 
-		mWebView.getSettings().setJavaScriptEnabled(true);
-		mWebView.setWebViewClient(new WebViewClient() {
-
-			@Override
-			public void onPageFinished(WebView view, String url) {
-				super.onPageFinished(view, url);
-
-				if ((url.matches("^http://localhost.*")) && (!mAuthInProgress)) {
-					mAuthInProgress = true;
-					mWebView.setVisibility(View.GONE);
-					mProgressView.setVisibility(View.VISIBLE);
-					// parse the URL to get the query parameters
-					Map<String, String> params = new HashMap<>();
-					try {
-						String query = new URL(url).getQuery();
-						String[] pairs = query.split("&");
-						for (String pair : pairs) {
-							int idx = pair.indexOf("=");
-							params.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"),
-								URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
-						}
-
-						if (params.get("state").matches(BuildConfig.CSRF_PROTECTION_STRING)) {
-							Map<String, String> queryMap = new ArrayMap<>();
-							queryMap.put("code", params.get("code"));
-							queryMap.put("redirect_uri", URLEncoder.encode(REDIRECT_URI, "utf-8"));
-							queryMap.put("client_id", BuildConfig.INOREADER_APP_ID);
-							queryMap.put("client_secret", BuildConfig.INOREADER_APP_KEY);
-							queryMap.put("scope", "");
-							queryMap.put("grant_type", "authorization_code");
-
-							AuthReplyHandler authReplyHandler = new AuthReplyHandler();
-							mRequest = mService.oauth2GetToken(queryMap)
-								.subscribeOn(Schedulers.io())
-								.observeOn(AndroidSchedulers.mainThread());
-							mLoginSubscription = mRequest.subscribe(authReplyHandler);
-
-						} else {
-							restartAuth(true);
-						}
-					} catch (MalformedURLException | UnsupportedEncodingException e) {
-						restartAuth(true);
-					}
-				}
-
-			}
-
-			@Override
-			public boolean shouldOverrideUrlLoading(WebView view, String url) {
-				view.loadUrl(url);
-				return true;
-			}
-		});
-
+		mIntent = getIntent();
 		restartAuth(false);
 	}
 
@@ -140,6 +80,44 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity {
 		}
 
 		super.onDestroy();
+	}
+
+	@Override
+	protected void onNewIntent(Intent intent) {
+		super.onNewIntent(intent);
+		mIntent = intent;
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		Uri uri = mIntent.getData();
+
+		if ((uri != null) && (uri.toString().startsWith(REDIRECT_URI))) {
+			try {
+				if (uri.getQueryParameter("state").matches(BuildConfig.CSRF_PROTECTION_STRING)) {
+					Map<String, String> queryMap = new ArrayMap<>();
+					queryMap.put("code", uri.getQueryParameter("code"));
+					queryMap.put("redirect_uri", URLEncoder.encode(REDIRECT_URI, "utf-8"));
+					queryMap.put("client_id", BuildConfig.INOREADER_APP_ID);
+					queryMap.put("client_secret", BuildConfig.INOREADER_APP_KEY);
+					queryMap.put("scope", "");
+					queryMap.put("grant_type", "authorization_code");
+
+					AuthReplyHandler authReplyHandler = new AuthReplyHandler();
+					mLoginSubscription = mService.oauth2GetToken(queryMap)
+						.subscribeOn(Schedulers.io())
+						.observeOn(AndroidSchedulers.mainThread())
+						.subscribe(authReplyHandler);
+
+				} else {
+					restartAuth(true);
+				}
+			} catch (UnsupportedEncodingException e) {
+				restartAuth(true);
+			}
+		}
 	}
 
 	private class AuthReplyHandler extends Subscriber<ResponseBody> {
@@ -158,13 +136,13 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity {
 
 		@Override
 		public void onNext(ResponseBody responseBody) {
-//			String response;
-//			try {
-//				response = responseBody.string();
-//			} catch (IOException e) {
-//				onError(e);
-//				return;
-//			}
+			String response;
+			try {
+				response = responseBody.string();
+			} catch (IOException e) {
+				onError(e);
+				return;
+			}
 //
 //			// Get the authentication token
 //			String holder[] = response.split("Auth=");
@@ -197,26 +175,27 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity {
 		}
 	}
 
-	private void restartAuth(Boolean showErrorMessage) {
-		mAuthInProgress = false;
-		mProgressView.setVisibility(View.GONE);
-		mWebView.setVisibility(View.VISIBLE);
-
-		try {
-			mWebView.loadUrl(OAUTH_URL +
-							 "?client_id=" + URLEncoder.encode(BuildConfig.INOREADER_APP_ID, "utf-8") +
-							 "&redirect_uri=" + URLEncoder.encode(REDIRECT_URI, "utf-8") +
-							 "&response_type=code" +
-							 "&scope=" + URLEncoder.encode(OPTIONAL_SCOPES, "utf-8") +
-							 "&state=" + URLEncoder.encode(BuildConfig.CSRF_PROTECTION_STRING, "utf-8"));
-		} catch (UnsupportedEncodingException e) {
-			// encoding is hard coded, so fix it
+	private void restartAuth(Boolean isError) {
+		if (isError) {
+			Snackbar.make(
+				mProgressView,
+				R.string.error_login,
+				Snackbar.LENGTH_LONG)
+				.show();
 		}
 
-		if (showErrorMessage) {
-			Snackbar
-				.make(mWebView, R.string.error_login, Snackbar.LENGTH_LONG)
-				.show();
+		try {
+			Intent intent = new Intent(
+				Intent.ACTION_VIEW,
+				Uri.parse(OAUTH_URL +
+						  "?client_id=" + URLEncoder.encode(BuildConfig.INOREADER_APP_ID, "utf-8") +
+						  "&redirect_uri=" + URLEncoder.encode(REDIRECT_URI, "utf-8") +
+						  "&response_type=code" +
+						  "&scope=" + URLEncoder.encode(OPTIONAL_SCOPES, "utf-8") +
+						  "&state=" + URLEncoder.encode(BuildConfig.CSRF_PROTECTION_STRING, "utf-8")));
+			startActivity(intent);
+		} catch (UnsupportedEncodingException e) {
+			// encoding is hard coded, so fix it
 		}
 	}
 }
